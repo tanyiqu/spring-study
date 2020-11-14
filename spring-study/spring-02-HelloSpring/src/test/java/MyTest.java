53f75688625267b0c5c604900f2d8e7bf5a4629d
Tanyiqu <1953649096@qq.com>
2020-11-14 13:54:49 +0800

    Hello Spring

---
+++ spring-study/spring-02-HelloSpring/src/test/java/MyTest.java
@@ -0,0 +1,16 @@
+import com.tanyiqu.pojo.Hello;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MyTest {
+    @Test
+    public void test() {
+        // 获取spring的上下文对象
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+        Hello hello = (Hello) context.getBean("hello");
+
+        System.out.println(hello);
+    }
+}
