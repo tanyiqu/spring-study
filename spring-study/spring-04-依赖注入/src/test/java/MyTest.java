6b1fa8e8ec273116558cb136367683c28011810a
Tanyiqu <1953649096@qq.com>
2020-11-17 11:12:40 +0800

    set方法依赖注入

---
+++ spring-study/spring-04-依赖注入/src/test/java/MyTest.java
@@ -0,0 +1,17 @@
+import com.tanyiqu.pojo.Student;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MyTest {
+
+    @Test
+    public void test() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+        Student student = (Student) context.getBean("student");
+
+        System.out.println(student);
+
+    }
+}
