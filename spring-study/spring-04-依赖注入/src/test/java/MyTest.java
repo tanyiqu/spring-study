de3b25116caea881b6444a81ccf30e97a0272dec
Tanyiqu <1953649096@qq.com>
2020-11-17 14:05:37 +0800

    拓展方法依赖注入

--- spring-study/spring-04-依赖注入/src/test/java/MyTest.java
+++ spring-study/spring-04-依赖注入/src/test/java/MyTest.java
@@ -1,4 +1,5 @@
 import com.tanyiqu.pojo.Student;
+import com.tanyiqu.pojo.User;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -14,4 +15,16 @@ public class MyTest {
         System.out.println(student);
 
     }
+
+    @Test
+    public void testP_C(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("userBeans.xml");
+        User userP = context.getBean("userP", User.class);
+        User userC = context.getBean("userC", User.class);
+        System.out.println(userP);
+        System.out.println(userC);
+    }
+
+
+
 }
