458b98020dcad07586bdb631ac22f49442fea8f2
Tanyiqu <1953649096@qq.com>
2020-11-18 12:49:59 +0800

    修改笔记

--- spring-study/spring-05-AutoWired/src/main/java/com/tanyiqu/pojo/Person.java
+++ spring-study/spring-05-AutoWired/src/main/java/com/tanyiqu/pojo/Person.java
@@ -1,6 +1,7 @@
 package com.tanyiqu.pojo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 import javax.annotation.Resource;
 
@@ -8,6 +9,7 @@ public class Person {
     String name;
 
     @Autowired
+    @Qualifier(value = "cat")
     Cat cat;
 
     @Resource(name = "dog")
